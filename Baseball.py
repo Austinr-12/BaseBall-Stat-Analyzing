1ebd6f9252f5175a90ff5fc9725a8ad59343716a
Austin <austin.ramirez1@outlook.com>
2026-02-09 14:33:07 -0600

    seeing babe ruth's stats

--- Baseball.py
+++ Baseball.py
@@ -6,3 +6,11 @@ active_players = batting[batting['AB'] > 0]
 active_players.describe()
 
 batting.groupby('playerID')['HR'].sum().sort_values(ascending=False)
+
+babe_ruth = batting[batting['playerID'] == 'ruthba01']
+
+start_year = babe_ruth['yearID'].min()
+start_year = babe_ruth['yearID'].max()
+
+ruth_years = batting[(batting['yearID'] >= start_year) & (batting['yearID'] <= end_year)]
+ruth_years.groupby('playerID')['HR'].sum().sort_values(ascending=False)
