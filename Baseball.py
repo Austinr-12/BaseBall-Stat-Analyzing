64696b7900ad33e3455c0e756a48173b535040b1
Austin <austin.ramirez1@outlook.com>
2026-02-09 14:27:15 -0600

    filtered out inactive players and grouped top performers

--- Baseball.py
+++ Baseball.py
@@ -1,2 +1,8 @@
 import pandas as pd
 
+batting = pd.read_csv('Batting.csv')
+
+active_players = batting[batting['AB'] > 0]
+active_players.describe()
+
+batting.groupby('playerID')['HR'].sum().sort_values(ascending=False)
