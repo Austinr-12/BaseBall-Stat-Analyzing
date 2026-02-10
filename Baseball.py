9580ab68d068bfa2d5dfc686527484578489b352
Austin <austin.ramirez1@outlook.com>
2026-02-10 14:56:30 -0600

    import matplot and getting total home runs over the years

--- Baseball.py
+++ Baseball.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 batting = pd.read_csv('Batting.csv')
 
@@ -14,3 +15,15 @@ start_year = babe_ruth['yearID'].max()
 
 ruth_years = batting[(batting['yearID'] >= start_year) & (batting['yearID'] <= end_year)]
 ruth_years.groupby('playerID')['HR'].sum().sort_values(ascending=False)
+
+
+total_hr_by_year = batting.groupby('yearID')['HR'].sum()
+plt.figure(figsize=(10, 6))
+plt.plot(total_hr_by_year.index, total_hr_by_year.values)
+
+plt.title('Total Home Runs Over Time')
+plt.xlabel('Year')
+plt.ylabel('Total Home Runs')
+
+plt.grid(True)
+plt.show()
