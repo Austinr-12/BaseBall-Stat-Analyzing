2cfc4ee6069cce0acaea410f67b51652c01eeba9
Austin <austin.ramirez1@outlook.com>
2026-02-23 12:37:54 -0600

    setup headers and function to get product details

---
+++ Amazon_WS/scraper.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import beautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.00; Win64; x64) AppleWebkit/537.36 (KHTML, like Gecho) Chrome/106.0.0.0 Safari/537.36'
+    'Accept-Language': 'en-US, en;q= 0.5'
+}
+
+def get_product_details(product_rul: str) -> dict:
+    pass
+    
