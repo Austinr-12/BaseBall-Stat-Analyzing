35962ec205ff82d63daf59619cab314a93637e2f
Austin <austin.ramirez1@outlook.com>
2026-02-27 14:03:48 -0600

    settinp up sate to update poem

--- Poem-Generation/PoemBox.jsx
+++ Poem-Generation/PoemBox.jsx
@@ -1,6 +1,11 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import ".env"
+
+
 export default function PoemBox() {
+    const [response, setResponse] = useState("");
+    const [error, setError] = useState(null);
+    const [currentTime, setCurrentTime] = useState(new Date());
   function fetchPoem() {
     try {
       const genAI = new GoogleGenerativeAI(API_KEY); // replace with your key
