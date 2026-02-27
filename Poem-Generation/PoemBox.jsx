76e9aa7d5fb79cb1cf925c46ecff0c0fba3c5370
Austin <austin.ramirez1@outlook.com>
2026-02-27 14:02:46 -0600

    function to fetch poem created

--- Poem-Generation/PoemBox.jsx
+++ Poem-Generation/PoemBox.jsx
@@ -1,2 +1,11 @@
-import { GoogleGnerativeAI } from "@google/generative-ai"
-
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import ".env"
+export default function PoemBox() {
+  function fetchPoem() {
+    try {
+      const genAI = new GoogleGenerativeAI(API_KEY); // replace with your key
+    }
+  } catch (err) {
+    // error handle here
+  }
+}
